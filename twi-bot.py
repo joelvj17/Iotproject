fc3d107312eea6f8e780fee3655400ff4465b7e2
joelvj17 <88233257+joelvj17@users.noreply.github.com>
2021-08-01 17:04:13 +0530

    Update twi-bot.py

--- twi-bot.py
+++ twi-bot.py
@@ -1,79 +1,74 @@
 from telegram.ext import Updater,MessageHandler,Filters
 ADAFRUIT_IO_USERNAME = "JOEL_V_J"
-ADAFRUIT_IO_KEY = "aio_TWjg62EWTwNja75Baf757WN2BtZm"
+ADAFRUIT_IO_KEY = "aio_upaU725BIpBxxyngmokmpW1MrjCR"
 def lighton(bot,update):
   chat_id=bot.message.chat_id
   path=''
-  Update.bot.sendPhoto(chat_id=chat_id,photo='https://static.scientificamerican.com/sciam/cache/file/2B38DE31-C1D3-4339-8808D61972976EE4.jpg')
-  bot.message.reply_text("The light is on")
+  update.bot.sendPhoto(chat_id=chat_id,photo='https://static.scientificamerican.com/sciam/cache/file/2B38DE31-C1D3-4339-8808D61972976EE4.jpg')
+  bot.message.reply_text("The light is on ")
   from Adafruit_IO import Client
-  aio = Client(ADAFRUIT_IO_USERNAME,ADAFRUIT_IO_KEY)
-  aio.send('light',100)
+  aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+  aio.send('light', 100)
   data = aio.receive('light')
-                     
   print('Received value: {0}'.format(data.value))
-  
- def lightoff(bot,update):
+
+def lightoff(bot,update):
   chat_id=bot.message.chat_id
   path=''
-  Update.bot.sendPhoto(chat_id=chat_id,photo='https://image.shutterstock.com/image-photo/light-bulb-turned-off-over-260nw-162882104.jpg')
+  update.bot.sendPhoto(chat_id=chat_id,photo='https://ak.picdn.net/shutterstock/videos/16051507/thumb/1.jpg')
   bot.message.reply_text("The light is off")
   from Adafruit_IO import Client
-  aio = Client('JOEL_V_J',ADAFRUIT_IO_KEY)
-  aio.send('light',0)
+  aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+  aio.send('light', 0)
   data = aio.receive('light')
   print('Received value: {0}'.format(data.value))
-  
- def fanon(bot,update):
+
+def fanon(bot,update):
   chat_id=bot.message.chat_id
   path=''
-  Update.bot.sendPhoto(chat_id=chat_id,photo='https://image.shutterstock.com/image-photo/ceiling-fan-rotating-room-electric-260nw-747740413.jpg')
+  update.bot.sendPhoto(chat_id=chat_id,photo="https://previews.123rf.com/images/llstock/llstock1909/llstock190900033/130060912-a-working-ceiling-fan-on-a-white-ceiling-close-up-with-blurred-fan-blades-three-lamps-daytime-.jpg")
   bot.message.reply_text("The fan is on")
   from Adafruit_IO import Client
-  aio = Client('JOEL_V_J',ADAFRUIT_IO_KEY)
-  aio.send('fan',100)
+  aio = Client(ADAFRUIT_IO_USERNAME,ADAFRUIT_IO_KEY)
+  aio.send('fan', 1)
   data = aio.receive('fan')
   print('Received value: {0}'.format(data.value))
-  
-  def fanoff(bot,update):
+
+def fanoff(bot,update):
   chat_id=bot.message.chat_id
   path=''
-  Update.bot.sendPhoto(chat_id=chat_id,photo='https://image.shutterstock.com/image-photo/black-ceiling-fan-on-white-260nw-1905057805.jpg')
+  update.bot.sendPhoto(chat_id=chat_id,photo='https://images-na.ssl-images-amazon.com/images/I/31fec2V1l4L._SX425_.jpg')
   bot.message.reply_text("The fan is off")
   from Adafruit_IO import Client
   aio = Client('JOEL_V_J',ADAFRUIT_IO_KEY)
-  aio.send('fan',0)
+  aio.send('fan', 0)
   data = aio.receive('fan')
   print('Received value: {0}'.format(data.value))
+
+def greeting(bot,update):
+  bot.message.reply_text("hi, what can i do for you?")
+  bot.message.reply_text("commands available only for fan and light now")
+
+def demo(bot,update):
+  a=bot.message.text
+  a=a.lower()
+  a=a.split()
+  if 'light' in a and 'on'in a:
+   lighton(bot,update)
+  elif 'light' in a and 'off'in a:
+    lightoff(bot,update)
+  elif 'fan' in a and 'on'in a:
+    fanon(bot,update)
+  elif 'fan' in a and 'off'in a:
+    fanoff(bot,update) 
+  elif 'hi' in a or 'hello'in a:
+    greeting(bot,update)
+  else:
+    bot.message.reply_text("INVALID COMMAND")
+
   
-  def greeting(bot,update):
-    bot.message.reply_text("Hi,what can i do for you?")
-    bot.message.reply_text("Commands only for light & fan")
-    
-    def demo(bot,update):
-      a=bot.message.text
-      a=a.lower()
-      a=a.split()
-      if 'light' in a and 'on'in a:
-        lighton(bot,update)
-        elif 'light' in a and 'off'in a:
-        lightoff(bot,update)
-       elif 'fan' in a and 'on'in a:
-        fanon(bot,update)
-         elif 'fan' in a and 'off'in a:
-        fanoff(bot,update)
-        elif 'hi' in a or 'hello' in a:
-          greeting(bot,update)
-         else:
-          bot.message.reply_text("INVALID COMMAND")
-          
-          
-          
-          
-          
-          
-          
-          
+  
+
 BOT_TOKEN='1904744734:AAECMOiaZsG3GYRsYMmXNKFwK8Bz2oNrbUs'
 u=Updater(BOT_TOKEN,use_context=True)
 dp=u.dispatcher
